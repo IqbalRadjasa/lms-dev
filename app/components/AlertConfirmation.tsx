4ef8e5d5dd2c591444ec8671ddb58ece10f77007
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-02-01 13:12:44 +0700

    refactor:Profile on header

--- app/components/AlertConfirmation.tsx
+++ app/components/AlertConfirmation.tsx
@@ -1,15 +1,4 @@
 import toast from 'react-hot-toast';
-import { Crimson_Text, Dongle } from 'next/font/google';
-
-const crimson = Crimson_Text({
-  subsets: ['latin'],
-  weight: ['400', '600', '700'],
-});
-
-const dongle = Dongle({
-  subsets: ['latin'],
-  weight: ['300', '400', '700'],
-});
 
 export const AlertConfirmation = (message: string, onConfirm: () => Promise<void> | void) => {
   toast.custom((t) => (
@@ -17,10 +6,10 @@ export const AlertConfirmation = (message: string, onConfirm: () => Promise<void
       className={`bg-white shadow-lg rounded-lg p-4 w-72 border
       ${t.visible ? 'animate-enter' : 'animate-leave'}`}
     >
-      <p className={`text-[#113F67] text-base mb-4 ${crimson.className}`}>{message}</p>
+      <p className={`text-[#113F67] text-sm mb-4`}>{message}</p>
 
       <div className="flex justify-end gap-2">
-        <button onClick={() => toast.remove(t.id)} className={`px-3 py-1 text-gray-800 text-sm rounded bg-gray-100 hover:bg-gray-300 ${crimson.className}`}>
+        <button onClick={() => toast.remove(t.id)} className={`px-3 py-1 text-gray-800 text-xs font-semibold rounded bg-gray-100 hover:bg-gray-300`}>
           Tidak
         </button>
 
@@ -29,7 +18,7 @@ export const AlertConfirmation = (message: string, onConfirm: () => Promise<void
             toast.remove(t.id);
             onConfirm();
           }}
-          className={`px-3 py-1 text-sm rounded bg-red-100 text-red-800 hover:bg-red-300 ${crimson.className} `}
+          className={`px-3 py-1 text-xs font-semibold rounded bg-red-100 text-red-800 hover:bg-red-300 `}
         >
           Ya
         </button>
--- app/dashboard/Header.tsx
+++ app/dashboard/Header.tsx
@@ -66,32 +66,34 @@ export default function Header({ user }: { user?: string }) {
   };
 
   return (
-    <header className="header h-18 flex items-center justify-between px-4">
+    <header className="header h-18 flex items-center justify-between px-4 py-3">
       <div className="ml-auto flex relative h-full cursor-pointer" ref={dropdownRef}>
         <div className="flex items-center">
           <ThemeToggle />
         </div>
-        <div onClick={() => setOpen(!open)} className="flex items-center border-l-2 border-[#d1d5db] text-[var(--text-secondary-light)] pl-3 ml-4">
+        {/* <div onClick={() => setOpen(!open)} className="flex items-center border-l-2 border-[#d1d5db] text-[var(--text-secondary-light)] pl-3 ml-4">
           <img src="/profil-dev.jpeg" className="w-8 h-auto rounded-full mr-2" />
-          <span className="regular-text uppercase mr-2">Muhammad Iqbal Radjasa</span>
+          <span className="regular-text uppercase mr-2">{user}</span>
           <svg className={`w-4 transition ${open ? 'rotate-180' : ''}`} viewBox="0 0 20 20">
             <path d="M5.5 7.5L10 12l4.5-4.5" stroke="currentColor" strokeWidth="2" />
           </svg>
-        </div>
+        </div> */}
 
         {/* Button */}
-        {/* <button onClick={() => setOpen(!open)} className="flex items-center px-3 py-1.5 rounded-full bg-white border-2 border-[#113F67] border not-active ml-16">
-          <img src="/profil-dev.jpeg" className="w-8 h-auto rounded-full mr-2" />
-          <span className="uppercase">Muhammad Iqbal Radjasa</span>
-          <span>{user ?? 'User'}</span>
+        <button onClick={() => setOpen(!open)} className="flex items-center px-3 py-2 rounded-[8px] bg-[var(--bg-color)] not-active ml-5">
+          <img src="/profil-dev.jpeg" className="w-8 h-auto rounded-full" />
+          <div className="flex flex-col items-start mx-3">
+            <span className="text-sm font-semibold text-primary-light">{user ?? 'User'}</span>
+            <span className="text-xs font-base">Admin</span>
+          </div>
           <svg className={`w-4 transition ${open ? 'rotate-180' : ''}`} viewBox="0 0 20 20">
             <path d="M5.5 7.5L10 12l4.5-4.5" stroke="currentColor" strokeWidth="2" fill="#113F67" />
           </svg>
-        </button> */}
+        </button>
 
         {/* Dropdown */}
         {open && (
-          <div className="absolute right-0 mt-18 w-40 bg-[var(--white)] rounded-md shadow-lg p-2 aniamte-in fade-in slide-in-from-top-1">
+          <div className="absolute right-0 mt-12 w-40 bg-[var(--white)] rounded-md shadow-lg p-2 aniamte-in fade-in slide-in-from-top-1">
             <button className={`w-full px-3 py-2 text-left rounded hover:bg-[#6c757d]/10 transition text-[#113F67]`}>
               <span className="regular-text text-[var(--text-secondary-light)]">Profil Saya</span>
             </button>
