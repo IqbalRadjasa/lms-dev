8feb8e3f01cd2dbc86744cdf3b3ad3d10df7cfd6
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-02-03 17:15:58 +0700

    feat:Show/hide password

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,32 +1,58 @@
 'use client';
 
+import { useState } from 'react';
+
 type InputProps = {
   label: string;
   type?: string;
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
+  showToogle?: boolean;
 };
 
-export default function Input({ label, type = 'text', value, onChange, placeholder }: InputProps) {
+export default function Input({ label, type = 'text', value, onChange, placeholder, showToogle }: InputProps) {
+  const [show, setShow] = useState(false);
+
+  const isPassword = type === 'password';
+  const inputType = isPassword && show ? 'text' : type;
+
   return (
     <div className="mb-4">
-      <label className={`block mb-1 text-sm text-black font-semibold text-primary-light`}>{label}</label>
-      <input
-        type={type}
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        placeholder={placeholder}
-        className="
-          text-primary-light font-base text-sm h-auto w-full
-          px-4 py-2
-          border rounded-sm
-          border-[var(--input-form-light)]
-          focus:outline-none
-          focus:ring-0
-          focus:border-[#25a194]
+      <label className="block mb-1 text-sm font-semibold text-primary-light">{label}</label>
+
+      <div className="relative">
+        <input
+          type={inputType}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder={placeholder}
+          className="
+        w-full
+        px-4 py-2 pr-10   /* space for eye */
+        text-sm
+        border rounded-sm
+        border-[var(--input-form-light)]
+        focus:outline-none
+        focus:ring-0
+        focus:border-[#25a194]
+        text-primary-light
+      "
+        />
+
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShow(!show)}
+            className="
+          absolute right-3 top-1/2 -translate-y-1/2
+          text-gray-500 hover:text-[#25a194]
         "
-      />
+          >
+            {show ? <i className="ri-eye-off-line text-lg" /> : <i className="ri-eye-line text-lg" />}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
