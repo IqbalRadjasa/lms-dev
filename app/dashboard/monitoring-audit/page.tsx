f367b47cc4ccce57102e6eafc754c30251bf12b7
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-04-29 14:01:55 +0700

    refactor: Export Modal

--- app/dashboard/monitoring-audit/page.tsx
+++ app/dashboard/monitoring-audit/page.tsx
@@ -128,10 +128,10 @@ export default function MonitoringAudit() {
                 <div className="input-group">
                   <label className="text-sm">Pilih Format: </label>
                   <div className="flex gap-2 mt-2">
-                    <span className={`${selectFormat == 1 ? 'bg-[var(--primary-600)] text-white' : 'bg-[var(--neutral-300)]'} px-3 py-2 rounded cursor-pointer transition`} onClick={() => setSelectFormat(1)}>
+                    <span className={`${selectFormat == 1 ? 'bg-[var(--primary-600)] text-white' : 'bg-[var(--neutral-300)]'} px-3 py-2 rounded cursor-pointer transition w-1/2 text-center text-sm`} onClick={() => setSelectFormat(1)}>
                       CSV
                     </span>
-                    <span className={`${selectFormat == 2 ? 'bg-[var(--danger-600)] text-white' : 'bg-[var(--neutral-300)]'} px-3 py-2 rounded cursor-pointer transition`} onClick={() => setSelectFormat(2)}>
+                    <span className={`${selectFormat == 2 ? 'bg-[var(--danger-600)] text-white' : 'bg-[var(--neutral-300)]'} px-3 py-2 rounded cursor-pointer transition w-1/2 text-center text-sm`} onClick={() => setSelectFormat(2)}>
                       PDF
                     </span>
                   </div>
